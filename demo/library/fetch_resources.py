fb92d441a78ce56a8461039edda636219e50c0f4
anis016 <anis.cuet016@gmail.com>
2018-05-15 16:05:16 +0200

    implemented the fetch and write function

--- demo/library/fetch_resources.py
+++ demo/library/fetch_resources.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python
+# A module that fetches a resource pointed to by a URL and then writes it to disk.
 
 from ansible.module_utils.basic import AnsibleModule
-
-# A module that fetches a resource pointed to by a URL and then writes it to disk.
+from ansible.module_utils.urls import open_url
+from urllib2 import URLError
 
 def save_data(mod):
     data = fetch(mod.params["url"])
     write(data, mod.params["dest"])
     mod.exit_json(msg="Data saved", changed=True)
 
+
+class FetchError(Exception):
+    pass
+
 def fetch(url):
-    print "Fetch something"
+    try:
+        stream = open_url(url)
+        return stream.read()
+    except URLError:
+        raise FetchError("Data could not be fetched.")
+
+
+class WriteError(Exception):
+    pass
 
 def write(data, dest):
-    print "Write the data"
+    try:
+        with open(dest, "w") as dest:
+            dest.write(data)
+    except IOError:
+        raise WriteError("Data could not be written.")
 
 def main():
     mod = AnsibleModule(
