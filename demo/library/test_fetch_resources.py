8ddedd702b59d77747bbef7b282ebdd67a818b77
anis016 <anis.cuet016@gmail.com>
2018-05-15 16:05:01 +0200

    added testcase for the function

--- demo/library/test_fetch_resources.py
+++ demo/library/test_fetch_resources.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# run command:
+# nosetests --doctest-tests -v demo/library/test_fetch_resources.py
+
 import unittest
 from ansible.compat.tests.mock import call, create_autospec, patch, mock_open
 from ansible.module_utils.basic import AnsibleModule
@@ -7,9 +10,9 @@ import fetch_resources as fetch_resources
 
 class TestFetchResource(unittest.TestCase):
 
-    @patch('fetch_resources.fetch')
     @patch('fetch_resources.write')
-    def test_save_data_path(self, fetch, write):
+    @patch('fetch_resources.fetch')
+    def test_save_data(self, fetch, write):
         # Setup
         mod_cls = create_autospec(AnsibleModule)
         mod = mod_cls.return_value
@@ -34,5 +37,39 @@ class TestFetchResource(unittest.TestCase):
         expected = call(msg="Data saved", changed=True)
         self.assertEqual(expected, mod.exit_json.call_args)
 
-# run command:
-# nosetests --doctest-tests -v demo/library/test_fetch_resources.py
+    @patch('fetch_resources.open_url')
+    def test_fetch(self, open_url):
+        # setup
+        url = "https://www.google.com"
+
+        # mock the return value of open_url
+        stream = open_url.return_value
+        stream.read.return_value = "<html><head></head><body>Hello</body></html>"
+        stream.getcode.return_value = 200
+        open_url.return_value = stream
+
+        # exercise
+        data = fetch_resources.fetch(url)
+
+        # verify
+        self.assertEqual(stream.read.return_value, data)
+        self.assertEqual(1, open_url.call_count)
+        expected = call(url)
+        self.assertEqual(expected, open_url.call_args)
+
+    def test_write(self):
+        # setup
+        data = "Somedata"
+        dest = "/tmp/testAnsible.txt"
+
+        # exercise
+        obj_open = "fetch_resources.open"
+        mok_open = mock_open()
+        with patch(obj_open, mok_open, create=True):
+            fetch_resources.write(data=data, dest=dest)
+
+        # verify
+        expected = call(dest, "w")
+        self.assertEqual(expected, mok_open.mock_calls[0])
+        expected = call().write(data)
+        self.assertEqual(expected, mok_open.mock_calls[2])
